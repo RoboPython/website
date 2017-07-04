dd878cc3bc3289ab1af1756683c4091f2a2e4b5f
Ruby Ferguson <rubydferguson@gmail.com>
2017-07-04 15:53:56 +0100

    fixes

--- flask_app.py
+++ flask_app.py
@@ -25,6 +25,6 @@ def contact():
 def page_not_found(e):
 	return render_template('404.html'),404
 
-#if __name__ == '__main__':
-#    app.run(debug=False)
+if __name__ == '__main__':
+    app.run(debug=False)
 
